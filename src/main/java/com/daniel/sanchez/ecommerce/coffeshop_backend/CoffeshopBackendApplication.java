878881d3743713144652ccc1380eed2d2baa0a03
danielsanchez18 <dsanchez151r@gmail.com>
2025-02-26 22:35:16 -0500

    first commit

---
+++ src/main/java/com/daniel/sanchez/ecommerce/coffeshop_backend/CoffeshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.sanchez.ecommerce.coffeshop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeshopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeshopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/sanchez/ecommerce/coffeshop_backend/CoffeshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.sanchez.ecommerce.coffeshop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeshopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
